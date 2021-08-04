f18149f51128c73ac5c506f8a2a10037e3405d7c
Rachel Solov <rsolov23@yahoo.com>
2021-08-04 15:07:08 -0500

    app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,54 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { ApolloProvider } from "@apollo/react-hooks";
+import ApolloClient from "apollo-boost";
 
-const ReactionList = ({ reactions }) => {
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import NoMatch from "./pages/NoMatch";
+import SingleThought from "./pages/SingleThought";
+import Profile from "./pages/Profile";
+import Signup from "./pages/Signup";
+
+const client = new ApolloClient({
+  request: (operation) => {
+    const token = localStorage.getItem("id_token");
+
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : "",
+      },
+    });
+  },
+  uri: "/graphql",
+});
+
+function App() {
   return (
-    <div className="card mb-3">
-      <div className="card-header">
-        <span className="text-light">Reactions</span>
-      </div>
-      <div className="card-body">
-        {reactions &&
-          reactions.map((reaction) => (
-            <p className="pill mb-3" key={reaction._id}>
-              {reaction.reactionBody}{" "}
-              <Link
-                to={`/profile/${reaction.username}`}
-                style={{ fontWeight: 700 }}
-              >
-                {reaction.username} on {reaction.createdAt}
-              </Link>
-            </p>
-          ))}
-      </div>
-    </div>
-  );
-};
+    <ApolloProvider client={client}>
+      <Router>
+        <div className="flex-column justify-flex-start min-100-vh">
+          <Header />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/signup" component={Signup} />
+              <Route exact path="/profile/:username?" component={Profile} />
+              <Route exact path="/thought/:id" component={SingleThought} />
 
-export default ReactionList;
+              <Route component={NoMatch} />
+            </Switch>
+          </div>
+          <Footer />
+        </div>
+      </Router>
+    </ApolloProvider>
+  );
+}
+
+export default App;
+
